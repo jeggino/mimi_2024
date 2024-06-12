1eaad0330a34f25b030200a9614d641440172b25
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-06-12 18:45:44 +0200

    Update app_garbage.py

--- app_garbage.py
+++ app_garbage.py
@@ -50,10 +50,9 @@ dict_values = {}
     
 for type in TYPE:
     with st.expander(type):
-        # for type_2 in dict_classes[type]:
         for type_2 in dict_classes[type]:        
             input = st.number_input(type_2,  step=1,  key=type + type_2, label_visibility="visible")
-            dict_values[type] = dict.fromkeys(type_2, input)
+            dict_values[type] = {type_2:input}
 
 st.number_input("Total weight",  step=1,  key="TOTAL WEIGHT", help=None, on_change=None, placeholder=None, disabled=False, label_visibility="visible")
 comment = st.text_input("Comment",)
