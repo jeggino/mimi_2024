4f15942291294a0f2ce5d66bf5ffd54500102214
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-06-13 12:22:28 +0200

    Update app_fish.py

--- app_fish.py
+++ app_fish.py
@@ -106,7 +106,6 @@ if selected == '📊':
 
     try:
         a = df.loc[option["selection"]["rows"][0]]["dict_values"]
-        
         df_3 = pd.DataFrame.from_dict(a, orient='index').stack().to_frame().rename(columns={0:"Ammount"})
         col_2.dataframe(df_3,use_container_width=True)
     except:
@@ -118,7 +117,6 @@ if selected == '📊':
         st.stop()
         
     col_1.warning("Are you sure you want to delete this observation?!")
-    
     submitted_2 = col_1.button("Yes I am sure!",key="submitted_2")
     if submitted_2:
         id = df.loc[option["selection"]["rows"][0]]["key"]
