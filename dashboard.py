de8e852a4a722c179796b96e32c6a1f483becdf0
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-06-14 09:03:28 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.switch_page("dashboard.py")
-st.switch_page("pages/fish_dashboard.py")
+if st.button("Home"):
+    st.switch_page("dashboard.py")
+if st.button("Page 1"):
+    st.switch_page("pages/fish_dashboard.py")
+
+
+
+
 
 st.write("Ciao peperone")
