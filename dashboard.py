e262950756d575eab4445eec3f9d923de3693fc8
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-06-14 10:12:43 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,8 +5,4 @@ st.sidebar.page_link("dashboard.py", label="", icon="🏠")
 st.sidebar.page_link("pages/fish_dashboard.py", label="", icon="🐟")
 st.sidebar.page_link("pages/dashboard_garbage.py", label="", icon="🗑️")
 
-
-
-
-
-st.write("Ciao peperone")
+st.image("https://siviaggia.it/wp-content/uploads/sites/2/2020/05/baia-di-ieranto.jpg")
