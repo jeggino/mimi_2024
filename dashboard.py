14c54bfe4bca8d1d0dfaf8f45df23978221c8bd4
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-06-14 08:51:09 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.sidebar.page_link("dashboard.py", label="Home")
-st.page_link("pages/fish_dashboard.py", label="Fish")
+st.sidebar.page_link("pages/fish_dashboard.py", label="Fish")
 
 st.write("Ciao peperone")
