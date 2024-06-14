53bb37ff6768efdc2ce7ace9b3e9589669bfe8af
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-06-14 10:15:31 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,19 @@
 import streamlit as st
 
+# --- CONFIGURATION ---
+st.set_page_config(
+    page_title="dashboard",
+    page_icon="🌊",
+    layout="wide",
+    
+)
+
+st.markdown("""
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+    </style>
+    """,
+    unsafe_allow_html=True)
 
 st.sidebar.page_link("dashboard.py", label="", icon="🏠")
 st.sidebar.page_link("pages/fish_dashboard.py", label="", icon="🐟")
