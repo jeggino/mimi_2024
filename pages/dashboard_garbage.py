6aeab36bd52fa3dbdb03fc3df6a3d7407319163f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-06-18 12:57:07 +0200

    Update dashboard_garbage.py

--- pages/dashboard_garbage.py
+++ pages/dashboard_garbage.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 import pandas as pd
 import datetime
@@ -12,6 +13,19 @@ st.set_page_config(
     layout="wide",
     
 )
+
+with st.sidebar:
+    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
+                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
+    
 
 
 st.markdown("""
@@ -36,60 +50,63 @@ db = deta.Base("df_garbage")
 def load_dataset():
   return db.fetch().items
 
-
-# --- APP ---
-col_1,col_2 = st.columns([4,3])
 db_content = load_dataset()
 df = pd.DataFrame(db_content)
-if len(df)==0:
-    st.warning("No data yet")
-    st.stop()
+
+# --- APP ---
+if chose == "About":
+    col_1,col_2 = st.columns([4,3])
     
-df_2 = df[["datum","location","day_storm","operator","total","comment"]]
-option = col_1.dataframe(data=df_2, width=None, height=None, use_container_width=True,
-             hide_index=True, column_order=None, column_config=None, key=None, on_select="rerun", selection_mode="single-row")
-
-try:
-    a = df.loc[option["selection"]["rows"][0]]["dict_values"]
-    df_3 = pd.DataFrame.from_dict(a, orient='index').stack().to_frame().rename(columns={0:"Ammount"})
-    col_2.dataframe(df_3,use_container_width=True)
-except:
-    col_1.warning("Select a row")
-    st.stop()
-
-submitted = col_1.toggle("Delete observation",key="submitted_1",value=False)
-if not submitted:
-    st.stop()
+    if len(df)==0:
+        st.warning("No data yet")
+        st.stop()
+        
+    df_2 = df[["datum","location","day_storm","operator","total","comment"]]
+    option = col_1.dataframe(data=df_2, width=None, height=None, use_container_width=True,
+                 hide_index=True, column_order=None, column_config=None, key=None, on_select="rerun", selection_mode="single-row")
     
-col_1.warning("Are you sure you want to delete this observation?!")
-submitted_2 = col_1.button("Yes I am sure!",key="submitted_2")
-if submitted_2:
-    id = df.loc[option["selection"]["rows"][0]]["key"]
-    db.delete(id)
-    st.rerun()
-
-df_concat = pd.DataFrame()
-
-for i in range(len(df)):
-    df_temp = pd.DataFrame.from_dict(df.dict_values[i], orient='index')\
-    .stack().to_frame().rename(columns={0:i})
+    try:
+        a = df.loc[option["selection"]["rows"][0]]["dict_values"]
+        df_3 = pd.DataFrame.from_dict(a, orient='index').stack().to_frame().rename(columns={0:"Ammount"})
+        col_2.dataframe(df_3,use_container_width=True)
+    except:
+        col_1.warning("Select a row")
+        st.stop()
     
-    df_concat= pd.concat([df_concat,df_temp],axis=1)
+    submitted = col_1.toggle("Delete observation",key="submitted_1",value=False)
+    if not submitted:
+        st.stop()
+        
+    col_1.warning("Are you sure you want to delete this observation?!")
+    submitted_2 = col_1.button("Yes I am sure!",key="submitted_2")
+    if submitted_2:
+        id = df.loc[option["selection"]["rows"][0]]["key"]
+        db.delete(id)
+        st.rerun()
 
-a = df_concat.stack().to_frame().reset_index()\
-.rename(columns={0:"value","level_2":"row"})
-
-a["datum"] = a["row"].apply(lambda x: df.loc[x,"datum"])
-a["location"] = a["row"].apply(lambda x: df.loc[x,"location"])
-a["day_storm"] = a["row"].apply(lambda x: df.loc[x,"day_storm"])
-
-a = a.groupby(['location',"level_0","level_1"],as_index=False)["value"].sum()
-
-import plotly.express as px
-df = px.data.tips()
-fig = px.sunburst(a, path=['location',"level_0","level_1"], values='value')
-
-st.plotly_chart(fig, use_container_width=True)
+elif chose == "Photo Editing":
+    df_concat = pd.DataFrame()
+    
+    for i in range(len(df)):
+        df_temp = pd.DataFrame.from_dict(df.dict_values[i], orient='index')\
+        .stack().to_frame().rename(columns={0:i})
+        
+        df_concat= pd.concat([df_concat,df_temp],axis=1)
+    
+    a = df_concat.stack().to_frame().reset_index()\
+    .rename(columns={0:"value","level_2":"row"})
+    
+    a["datum"] = a["row"].apply(lambda x: df.loc[x,"datum"])
+    a["location"] = a["row"].apply(lambda x: df.loc[x,"location"])
+    a["day_storm"] = a["row"].apply(lambda x: df.loc[x,"day_storm"])
+    
+    a = a.groupby(['location',"level_0","level_1"],as_index=False)["value"].sum()
+    
+    import plotly.express as px
+    df = px.data.tips()
+    fig = px.sunburst(a, path=['location',"level_0","level_1"], values='value')
+    
+    st.plotly_chart(fig, use_container_width=True)
 
     
    
