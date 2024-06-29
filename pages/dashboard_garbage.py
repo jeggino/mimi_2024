9ff28032af95ef412a833d3949eff43cf7793b8b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-06-29 08:25:07 +0200

    Update dashboard_garbage.py

--- pages/dashboard_garbage.py
+++ pages/dashboard_garbage.py
@@ -112,7 +112,7 @@ elif choose == "Sunburst chart":
     st.plotly_chart(fig_2, use_container_width=True)
     "---"
     df_bar_chart = df_2.groupby('location',as_index=False)['total'].sum().sort_values(by='total',ascending=False)
-    st.bar_chart(df_bar_chart,x="total", y="total",horizontal=True)
+    st.bar_chart(df_bar_chart,x="total", y="location",horizontal=True)
     # st.dataframe(data=df_2.groupby('location',as_index=False)['total'].sum(), use_container_width=True,hide_index=True)
     
 
